49afe4b99e5b2635acd42f43e718e4b309710293
aripan <aripan017@gmail.com>
2020-09-29 15:24:46 +0200

    modified client ID

--- config.js
+++ config.js
@@ -2,7 +2,16 @@ module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: process.env.MONGO_URL,
   facebook: {
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientId: "871102893421042",
+    clientSecret: "70525ec0161b0e0485d61b59314fdcf5",
   },
 };
+
+// module.exports = {
+//   secretKey: "12345-67890-09876-54321",
+//   mongoUrl: process.env.MONGO_URL,
+//   facebook: {
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//   },
+// };
