3ecabdbeb08f66c428a30bac817874593d038075
aripan <aripan017@gmail.com>
2020-09-30 00:45:31 +0200

    modified secure port

--- app.js
+++ app.js
@@ -51,17 +51,17 @@ connect.then(
 
 let app = express();
 
-// Secure traffic only
-app.all("*", (req, res, next) => {
-  if (req.secure) {
-    return next();
-  } else {
-    res.redirect(
-      307,
-      "https://" + req.hostname + ":" + app.get("secPort") + req.url
-    );
-  }
-});
+// // Secure traffic only
+// app.all("*", (req, res, next) => {
+//   if (req.secure) {
+//     return next();
+//   } else {
+//     res.redirect(
+//       307,
+//       "https://" + req.hostname + ":" + app.get("secPort") + req.url
+//     );
+//   }
+// });
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
