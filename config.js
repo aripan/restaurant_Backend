634e6f439979d64bd290895f4d68de216971dc88
aripan <aripan017@gmail.com>
2020-09-29 15:49:11 +0200

    modified mongoose

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
-  mongoUrl: process.env.MONGO_URL,
+  mongoUrl:
+    "mongodb+srv://Asad123:Asad123@rescluster.kfqqb.mongodb.net/test?retryWrites=true&w=majority",
   facebook: {
     clientId: "871102893421042",
     clientSecret: "70525ec0161b0e0485d61b59314fdcf5",
