52e46eccabb7c6af7b08e7ce6572c1a601a6db5c
aripan <aripan017@gmail.com>
2020-09-30 01:12:49 +0200

    Prepared & Deployed

--- config.js
+++ config.js
@@ -1,18 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
-  mongoUrl:
-    "mongodb+srv://Asad123:Asad123@rescluster.kfqqb.mongodb.net/test?retryWrites=true&w=majority",
+  mongoUrl: process.env.MONGO_URL,
   facebook: {
-    clientId: "871102893421042",
-    clientSecret: "70525ec0161b0e0485d61b59314fdcf5",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
   },
 };
-
-// module.exports = {
-//   secretKey: "12345-67890-09876-54321",
-//   mongoUrl: process.env.MONGO_URL,
-//   facebook: {
-//     clientId: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//   },
-// };
